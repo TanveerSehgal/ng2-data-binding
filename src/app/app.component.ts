ff9d77f161b6983634405e8ad50b4e6a9ed964bd
Tanveer Sehgal <tanveersehgal@gmail.com>
2016-12-06 00:56:38 +0700

    Add 2 way data binding example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,10 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent {
-    title = 'Angular 2 Data Binding';
-    purpose = 'You will learn how to bind data to your view in Angular 2';
-    widgetTwoDisabled = true;
+    public title : string = 'Angular 2 Data Binding';
+    public purpose : string = 'You will learn how to bind data to your view in Angular 2';
+    public widgetTwoDisabled : boolean = true;
+    public inputWidgetValue : string = 'edit me!';
 
     toggleWidgetTwo() {
         this.widgetTwoDisabled = !this.widgetTwoDisabled;
